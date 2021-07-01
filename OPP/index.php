865ad1180cc20fcdc588579e0808b5ea9787ac53
CrisChiera <cristianchiera@gmail.com>
2021-07-01 15:52:48 -0300

    Create index.php

---
+++ OPP/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$nombre='Cristian';
+$apellido='Chiera';
+$hobbie='Javascript';
+$editor='Visual Code, PHPStorm';
+$os='Manjaro,Ubuntu,Win';
+
+echo 'Nombre: ',$nombre,'<br>';
+echo 'Apellido: ',$apellido,'<br>';
+echo 'Hobbie: ',$hobbie,'<br>';
+echo 'Editor de código preferido: ',$editor,'<br>';
+echo 'Sistema operativo preferido: ',$os,'<br>';
+
+
+ ?>
